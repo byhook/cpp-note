799ee374dfa0032bb7d01ed480bf3e092bef6346
handyzhou <byhook@163.com>
2021-01-13 13:53:33 +0800

    vector存储string测试

--- chapter2/get_line.cpp
+++ chapter2/get_line.cpp
@@ -2,8 +2,6 @@
 #include <fstream>
 
 int main() {
-    std::cout << "hello chapter2" << std::endl;
-    system("pwd");
     std::ifstream in("../res/main.txt");
     std::string line;
     while (std::getline(in,line)){
