f4d03f9cf216a306b8e2e90e98b04c6cbe7e745b
byhook <byhook@163.com>
2022-10-16 21:22:41 +0800

    函数指针

--- cpp_primer_plus/chapter_7/main.cpp
+++ cpp_primer_plus/chapter_7/main.cpp
@@ -6,7 +6,41 @@ struct travel_time {
     int minutes;
 };
 
-travel_time sum(travel_time t1, travel_time t2);
+/**
+ * 这里局部变量返回结构体
+ * 其实是另一个拷贝
+ * @param t1
+ * @param t2
+ * @return
+ */
+travel_time sum(travel_time t1, travel_time t2) {
+    travel_time total;
+    total.hours = t1.hours + t2.hours;
+    total.minutes = t1.minutes + t2.minutes;
+    //获取当前局部变量结构体地址
+    std::cout << "total:" << &total << std::endl;
+    return total;
+}
+
+/**
+ * 7.10 函数指针
+ */
+int think(int value){
+    return value + 100;
+}
+
+//声明函数指针
+int (*funPtr)(int value);
+
+void process(int value, int (*funPtr)(int )){
+    //int result = (*funPtr)(value); //和下面的等价
+    int result = funPtr(value);
+    std::cout << "funPtr:" << result << std::endl;
+}
+
+void thought(int value){
+    std::cout << "thought:" << value << std::endl;
+}
 
 int main() {
 
@@ -24,7 +58,7 @@ int main() {
     int target = 99;
     int age = 30;
     //这里的&取地址
-    const int * const_ptr = &age;
+    const int *const_ptr = &age;
     std::cout << "const_ptr age address:" << const_ptr << std::endl;
     const_ptr = &target; //合法
     //*const_ptr = target; //非法
@@ -38,21 +72,8 @@ int main() {
     std::cout << "ptr_const age value:" << *ptr_const << std::endl;
     //int * const pt 是指针常量((int *)(const pt)): 可以改内容,不能改指针的地址
 
+    //7.10 函数指针
+    process(10,think);
+    thought(think(100));
     return 0;
-}
-
-/**
- * 这里局部变量返回结构体
- * 其实是另一个拷贝
- * @param t1
- * @param t2
- * @return
- */
-travel_time sum(travel_time t1, travel_time t2) {
-    travel_time total;
-    total.hours = t1.hours + t2.hours;
-    total.minutes = t1.minutes + t2.minutes;
-    //获取当前局部变量结构体地址
-    std::cout << "total:" << &total << std::endl;
-    return total;
 }
