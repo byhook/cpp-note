2c3cc998cadf5b37fe7ea39c5adfde7d65780edd
byhook <byhook@163.com>
2022-10-16 13:44:17 +0800

    指针常量和常量指针的区别

--- cpp_primer_plus/chapter_7/main.cpp
+++ cpp_primer_plus/chapter_7/main.cpp
@@ -6,18 +6,37 @@ struct travel_time {
     int minutes;
 };
 
-travel_time sum(travel_time t1,travel_time t2);
+travel_time sum(travel_time t1, travel_time t2);
 
 int main() {
 
+    //7.3
     std::cout << "Hello, World!" << std::endl;
-    travel_time day1 = {1,10};
-    travel_time day2 = {2,20};
+    travel_time day1 = {1, 10};
+    travel_time day2 = {2, 20};
     travel_time result = sum(day1, day2);
 
     std::cout << "result.hours:" << result.hours << std::endl;
     //获取返回的结构体地址
     std::cout << "total:" << &result << std::endl;
+
+    //7.3.5-指针和const
+    int target = 99;
+    int age = 30;
+    //这里的&取地址
+    const int * const_ptr = &age;
+    std::cout << "const_ptr age address:" << const_ptr << std::endl;
+    const_ptr = &target; //合法
+    //*const_ptr = target; //非法
+    std::cout << "const_ptr age address:" << const_ptr << std::endl;
+    //const int * pt 是常量指针((const int)(* pt)): 可以改指针的地址,不能改内容
+
+    int *const ptr_const = &age;
+    std::cout << "ptr_const age address:" << ptr_const << std::endl;
+    //ptr_const = &target; 　//非法
+    *ptr_const = target;   //合法
+    std::cout << "ptr_const age value:" << *ptr_const << std::endl;
+    //int * const pt 是指针常量((int *)(const pt)): 可以改内容,不能改指针的地址
 
     return 0;
 }
@@ -29,7 +48,7 @@ int main() {
  * @param t2
  * @return
  */
-travel_time sum(travel_time t1,travel_time t2){
+travel_time sum(travel_time t1, travel_time t2) {
     travel_time total;
     total.hours = t1.hours + t2.hours;
     total.minutes = t1.minutes + t2.minutes;
