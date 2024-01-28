6fe0ed2560dc6a427cdce759042138120ac8f159
byhook <byhook@163.com>
2024-01-28 17:48:57 +0800

    统一目录结构

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    int number;
-    std::cin >> number;
-    std::cout << std::hex << number << std::endl;
-    return 0;
-}
